18f345efffa42e8a9359da14278fa205d4c842da
chen <13683521923@sina.cn>
2019-01-03 10:21:10 +0800

    'submit'

---
+++ js/tfboys-.js
@@ -0,0 +1,16 @@
+﻿	var currentSwiper = new Swiper('.current-swiper',{
+		direction: 'horizontal',
+		loop : true,
+		speed:1000,
+		autoplay:true,
+	});		
+	var leftSwiper = new Swiper('.left-swiper',{
+		loop : true,
+		simulateTouch : false,
+	});
+	var rightSwiper = new Swiper('.right-swiper',{
+		loop : true,
+		simulateTouch : false,
+	});			
+	currentSwiper.controller.control = [leftSwiper,rightSwiper];
+	
